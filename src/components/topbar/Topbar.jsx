c51a07adf99e55ac8e6dafbdf2e213a081229f85
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-04-23 20:20:40 +0530

    Updated

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,28 @@
 import { MenuOpen } from '@mui/icons-material';
 import './topbar.scss'
-import AnalyticsIcon from '@mui/icons-material/Analytics';
+import MailIcon from '@mui/icons-material/Mail';
+import {Avatar} from '@mui/material';
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
-        <div className="left">
+        <div className="left"> 
           <a href="#intro" className="logo">
-          </a>
+          </a>   
+          <Avatar
+              //src="/assets/1.jpg"
+              src="https://maheshabeykoon.github.io/assets/img/profile-img.jpg"
+              sx={{ width: 40, height: 40 }}
+            />
           <div className="itemContainer">
-            <AnalyticsIcon className="icon"/>
-            <span>Mahesh</span>
+            <MailIcon className="icon"/>
+            <span>maheshpabeykoon@gmail.com</span>
           </div>
-          <div className="itemContainer">
+          {/* <div className="itemContainer">
             <AnalyticsIcon className="icon"/>
-            <span>0717575</span>
-          </div>
+            <span>maheshpabeykoon@gmail.com</span>
+          </div> */}
         </div>
         <div className="right">
           <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
