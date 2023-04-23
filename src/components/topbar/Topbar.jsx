a8b367037d1e75fca63540d24f05b0fa7ebb0ef7
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-04-23 17:28:18 +0530

    Added drawer navigation  & updated the top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,31 @@
+import { MenuOpen } from '@mui/icons-material';
 import './topbar.scss'
+import AnalyticsIcon from '@mui/icons-material/Analytics';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">Introd</a>
-
+          <a href="#intro" className="logo">
+          </a>
+          <div className="itemContainer">
+            <AnalyticsIcon className="icon"/>
+            <span>Mahesh</span>
+          </div>
+          <div className="itemContainer">
+            <AnalyticsIcon className="icon"/>
+            <span>0717575</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
