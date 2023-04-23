1edb4b8e1e5a26f26ee8cd92f993c4381e9534d9
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-04-23 23:34:40 +0530

    Add introductory section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,43 @@
 import  './intro.scss';
+import img from '../assets/1.png'
+import arrow from '../assets/arrow.png'
+import {init} from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+  
+  const textRef = useRef();
+  
+  useEffect(() => {
+    // console.log(textRef);
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Web App","Mobile App"]
+    })
+  }, [])
+
   return (
-    <div className="intro" id="intro"> 
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src={img} />
+        </div>
+      </div> 
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there I'm</h2>
+          <h1>Mahesh Abeykoon</h1>
+          <h3>
+            <span ref={textRef}></span> Developer
+            </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src={arrow} alt=''/>
+
+        </a>
+      </div>
 
     </div>
 
