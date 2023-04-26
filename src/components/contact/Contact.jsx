4062e6b6bb3de3b1a2c8eea14bc5acc5e87afedf
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-04-26 22:59:51 +0530

    Updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,4 @@
-// import { useState } from 'react';
-// import './contact.scss'
-
-// export default function Contact() {
-
-//   const [message, setMessage] = useState(false)
-
-//   const handleSubmit = (e)=>{
-//     e.preventDefault();
-//     setMessage(true);
-//   }
-
-//   return (
-//     <div className="contact" id="contact">
-//       <div className="left">
-//         <img src= "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.I0G-374jx9SPUFRnVQaCmAHaE8%26pid%3DApi&f=1&ipt=da2df203919da8057b7203e8636b176557105bc0617900e679e6fdd05642ee6f&ipo=images"/>
-//       </div>
-      
-//       <div className="right">
-//         <h2>Contact.</h2>
-//         <form onSubmit={handleSubmit}>
-//           <input type='text' placeholder='Email'/>
-//           <textarea placeholder='Message'></textarea> 
-//           <button type="submit">Send</button>
-//           {message && <span>Thanks, I'll reply</span>}
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
-import React from "react";
 import { FaEnvelope, FaPhone, FaLinkedin, FaGithub } from "react-icons/fa";
-//import contactImg from "../images/contact-img.jpg";
 import './contact.scss'
 
 export default function Contact() {
@@ -40,10 +7,6 @@ export default function Contact() {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            {/* <p>
-              If you would like to get in touch, please fill in the form below
-              and I will get back to you as soon as possible.
-            </p> */}
           
           </div>
           <div className="col-md-6">
