50963cfbfa59c3700cd67bc12a39cfa9b38c61fc
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-05-01 21:00:12 +0530

    Add skills section to the menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,6 +8,9 @@ export default function Menu({ menuOpen, setMenuOpen }){
                     <a href="#intro">Home</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#skills">Technical Skills</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#portfolio">Portolio</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
