5dd8820de5b296fe79e894164bb67eb187c2c3a7
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-05-01 20:00:13 +0530

    Add social icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,7 @@ import './topbar.scss'
 import MailIcon from '@mui/icons-material/Mail';
 import {Avatar} from '@mui/material';
 import img from '../assets/11.jpg'
+import { FaLinkedin, FaGithub, FaMedium ,FaInstagram, FaFacebook} from "react-icons/fa";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
@@ -19,6 +20,24 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
             <MailIcon className="icon"/>
             <span>maheshpabeykoon@gmail.com</span>
           </div>
+          <div className= 'contact-info'>
+              <a className='fa-icon' href="https://github.com/Mahesh-Abeykoon">
+                <div><FaGithub /> </div>
+              </a>
+              <a className='fa-icon' href="https://www.linkedin.com/in/mahesh-abeykoon">
+                <div><FaLinkedin /> </div>
+              </a>
+              <a className='fa-icon' href="https://medium.com/@maheshoabeykoon">
+                <div><FaMedium /> </div>
+              </a>
+              <a className='fa-icon' href="https://instagram.com/mahe__sh">
+                <div><FaInstagram /> </div>
+              </a>
+              <a className='fa-icon' href="https://www.facebook.com/Mahesh0Abeykoon">
+                <div><FaFacebook /> </div>
+              </a>
+          </div>
+         
           {/* <div className="itemContainer">
             <AnalyticsIcon className="icon"/>
             <span>maheshpabeykoon@gmail.com</span>
