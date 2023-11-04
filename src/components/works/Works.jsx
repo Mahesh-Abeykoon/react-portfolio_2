b40c4238d3b3f51823eb16309ad0cdee4d380bb2
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-11-04 23:35:32 +0530

    Changes

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -11,33 +11,37 @@ export default function Works() {
     {
       id:"1",
       icon: require("../assets/dysM.png"),
-      title: "DysM",
+      title: "Mobile App",
       des: "Final year research project focused on creating a mobile application to assist dyslexic children.",
       img: require("../assets/dysM.png"),
+      projects: "https://github.com/Mahesh-Abeykoon/dyslexia-app-for-kids",
     },
 
     {
       id:"2",
       icon: require("../assets/englishApp.png"),
       title: "English Learning App",
-      des: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, ratione",
+      des: "Developed an English Learning Android application using React-Native",
       img: require("../assets/englishApp.png"),
+      projects: "https://github.com/Mahesh-Abeykoon/smart-english-learningApp",
     },
 
     {
       id:"3",
       icon: require("../assets/unity3d.png"),
       title: "Unity Game",
-      des: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, ratione",
+      des: "This is a simple and attractive Unity 3D Game",
       img: require("../assets/unity3d.png"),
+      projects: "https://github.com/Mahesh-Abeykoon/unity-roll-a-ball_mini-game/tree/master",
     },
 
     {
       id:"4",
       icon: require("../assets/mernApp.png"),
       title: "Mern App",
-      des: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, ratione",
+      des: "Created a web application enabling users to post articles",
       img: require("../assets/mernApp.png"),
+      projects: "https://github.com/Mahesh-Abeykoon/MERN-blog-application",
     },
   ]
 
@@ -59,7 +63,7 @@ export default function Works() {
                 </div>
                 <h2>{d.title}</h2>
                 <p>{d.des}</p>
-                <span>Projects</span>
+                <span>{d.projects}</span>
               </div>
             </div>
             <div className="right">
