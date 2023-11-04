86513a85b42673e6ebf8c3aa9d13538f7c1efaed
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-11-04 23:35:14 +0530

    Change the data

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 export const portfolioMobile = [
     {
         id: 1,
-        title: "English App",
+        title: "Home",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/app1.jpg"
     },
     {
         id: 2,
-        title: "Fitness App",
+        title: "Learning Menu",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/app6.jpg"
     },
     {
         id: 3,
-        title: "Dyslexia App",
+        title: "Tenses Menu",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/app8.jpg"
     },
@@ -23,19 +23,20 @@ export const portfolioMobile = [
 export const portfolioWeb = [
     {
         id: 10,
-        title: "MERN Web",
-        img:
-        "https://maheshabeykoon.github.io/assets/img/interface/web7.png"
-    },
-    {
-        id: 20,
-        title: "WEB 2",
+        title: "MERN Blog Home",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/web6.png"
     },
     {
+        id: 20,
+        title: "Blog Posting",
+        img:
+        "https://maheshabeykoon.github.io/assets/img/interface/web7.png"
+        
+    },
+    {
         id: 30,
-        title: "WEB 3",
+        title: "Signup",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/web1.png"
     },
@@ -46,25 +47,25 @@ export const portfolioWeb = [
 export const portfolioGame = [
     {
         id: 100,
-        title: "Unity 3D Game",
+        title: "Unity Game Engine",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/Game1.png"
     },
     {
         id: 200,
-        title: "Unity 3D Game",
+        title: "Roll a ball game",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/Game2.png"
     },
     {
         id: 300,
-        title: "Unity 3D Game",
+        title: "Game Menu",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/Game3.png"
     },
     {
         id: 400,
-        title: "Unity 3D Game",
+        title: "Passing Levels",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/Game4.png"
     },
@@ -73,25 +74,25 @@ export const portfolioGame = [
 export const portfolioOther = [
     {
         id: 11,
-        title: "Other",
+        title: "Home Menu (CMS)",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/welcome%200.jpg"
     },
     {
         id: 12,
-        title: "Other",
+        title: "Login UI (CMS)",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/patient/patient%20log%201.jpg"
     },
     {
         id: 13,
-        title: "Other",
+        title: "Make Appointment",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/patient/patient%20deatails%20adding%20panal%20(2).jpg"
     },
     {
         id: 14,
-        title: "Other",
+        title: "Portfolio",
         img:
         "https://maheshabeykoon.github.io/assets/img/interface/portfolio2.png"
     },
