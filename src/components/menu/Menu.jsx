f80ca81e24d921e598d912d0a487229cedadd623
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-11-05 19:07:52 +0530

    Updates

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -11,7 +11,7 @@ export default function Menu({ menuOpen, setMenuOpen }){
                     <a href="#skills">Technical Skills</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
-                    <a href="#portfolio">Portolio</a>
+                    <a href="#portfolio">Portfolio</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
                     <a href="#works">Works</a>
